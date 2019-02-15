cdf0b29236df46ec4062a5e5ecb52d9ab90479be
Alexandre Kirszenberg <alexandre.kirszenberg@epita.fr>
2019-02-15 02:04:29 +0100

    Header tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -175,11 +175,17 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   menuIcon: {
-    position: "absolute",
-    left: theme.spacing(1),
+    flexShrink: 0,
   },
   verticalSpacer: {
     height: theme.spacing(2),
+  },
+  buttonContainer: {
+    marginLeft: theme.spacing(-1),
+    marginRight: theme.spacing(1),
+    display: "flex",
+    alignItems: "center",
+    width: 0,
   },
   headerContainer: {
     paddingTop: theme.spacing(2),
@@ -188,9 +194,10 @@ const useStyles = makeStyles((theme: Theme) => ({
     flexDirection: "column",
     alignItems: "stretch",
     [theme.breakpoints.down("sm")]: {
+      flexDirection: "row",
       paddingTop: theme.spacing(1),
       paddingBottom: theme.spacing(1),
-      alignItems: "center",
+      justifyContent: "center",
     },
   },
   menu: {
@@ -205,6 +212,11 @@ const useStyles = makeStyles((theme: Theme) => ({
     alignItems: "center",
   },
   logoContainer: {
+    display: "flex",
+    justifyContent: "center",
+    flexGrow: 1,
+  },
+  logoButton: {
     borderRadius: theme.shape.borderRadius,
   },
   logo: {
@@ -258,12 +270,14 @@ export default function Header() {
         <Headroom>
           <header className={styles.header}>
             <Container className={styles.headerContainer}>
-              <IconButton
-                onClick={() => setDrawerOpen(true)}
-                className={styles.menuIcon}
-              >
-                <MenuIcon />
-              </IconButton>
+              <div className={styles.buttonContainer}>
+                <IconButton
+                  onClick={() => setDrawerOpen(true)}
+                  className={styles.menuIcon}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </div>
 
               <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
                 <div tabIndex={0} role="button">
@@ -274,23 +288,25 @@ export default function Header() {
                 </div>
               </Drawer>
 
-              <Link to="/">
-                <ButtonBase classes={{ root: styles.logoContainer }}>
-                  <img
-                    src={require("@/images/logo_clear.svg")}
-                    alt="Logo"
-                    className={styles.logo}
-                  />
+              <div className={styles.logoContainer}>
+                <Link to="/">
+                  <ButtonBase classes={{ root: styles.logoButton }}>
+                    <img
+                      src={require("@/images/logo_clear.svg")}
+                      alt="Logo"
+                      className={styles.logo}
+                    />
 
-                  <div className={styles.logoSpacer} />
+                    <div className={styles.logoSpacer} />
 
-                  <img
-                    src={require("@/images/logo_text.svg")}
-                    alt="IMAGE"
-                    className={styles.logoText}
-                  />
-                </ButtonBase>
-              </Link>
+                    <img
+                      src={require("@/images/logo_text.svg")}
+                      alt="IMAGE"
+                      className={styles.logoText}
+                    />
+                  </ButtonBase>
+                </Link>
+              </div>
             </Container>
           </header>
         </Headroom>
@@ -301,7 +317,7 @@ export default function Header() {
           <Container className={styles.headerContainer}>
             <div className={styles.top}>
               <Link to="/">
-                <ButtonBase classes={{ root: styles.logoContainer }}>
+                <ButtonBase classes={{ root: styles.logoButton }}>
                   <img
                     src={require("@/images/logo_clear.svg")}
                     alt="Logo"
